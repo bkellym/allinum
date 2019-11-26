86aac49314512eceb74798fd62307a4d0a4832a0
bkellym <bkellym.sm@gmail.com>
2019-11-26 13:42:32 -0300

    DEV 007: Implementa controle de Login
    Descrição: Bloqueia acesso de usuários que não estejam logados, e define perfil de super usuário e Staff para acesso

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^usuario_cadastro/$', usuario_cadastro, name='usuario_cadastro'),
     url(r'^usuario_delete/(?P<pk>[0-9]+)', usuario_delete, name='usuario_delete'),
     url(r'^usuario_lista/$', usuario_lista, name='usuario_lista'),
-    url(r'^login/$', efetuar_login, name='login')
+    url(r'^login/$', efetuar_login, name='login'),
+    url(r'^logout/$', efetuar_logout, name='logout')
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
@@ -7,22 +8,30 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
+@login_required
 def usuario_cadastro(request, template_name="usuario_cadastro.html"):
-    if request.method == "POST":
-        username = request.POST['username']
-        email = request.POST['email']
-        password = request.POST['password']
-        tipo = request.POST['tipo_usuario']
+    user = request.user
 
-        user = User.objects.create_user(username, email, password)
-        if tipo == "administrador":
-            user.is_staff = True
-            user.save()
+    if user.is_staff:
+        if request.method == "POST":
+            username = request.POST['username']
+            email = request.POST['email']
+            password = request.POST['password']
+            tipo = request.POST['tipo_usuario']
 
-        return redirect('/usuario_lista/')
+            user = User.objects.create_user(username, email, password)
+            if tipo == "administrador":
+                user.is_staff = True
+                user.save()
+
+            return redirect('/usuario_lista/')
+    else:
+        return HttpResponse("Permissão Negada!")
 
     return render(request, template_name, {})
 
+
+@login_required
 def usuario_delete(request, pk, template_name='usuario_delete.html'):
     user = request.user
 
@@ -40,10 +49,13 @@ def usuario_delete(request, pk, template_name='usuario_delete.html'):
 
     return render(request, template_name, {'usuario':usuario})
 
+
+@login_required
 def usuario_lista(request, template_name="usuario_lista.html"):
     usuarios = User.objects.all()
     usuario = {'lista': usuarios}
     return render(request, template_name, usuario)
+
 
 def efetuar_login(request, template_name="login.html"):
     next = request.GET.get('next', '/usuario_lista/')
@@ -59,3 +71,8 @@ def efetuar_login(request, template_name="login.html"):
             return HttpResponseRedirect(settings.LOGIN_URL)
 
     return render(request, template_name, {'redirect_to': next})
+
+
+def efetuar_logout(request):
+    logout(request)
+    return HttpResponseRedirect(settings.LOGIN_URL)
