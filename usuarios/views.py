1b633e99ff2ee496d209dafd5d2a9a6d606aab26
bkellym <bkellym.sm@gmail.com>
2019-12-04 17:50:36 -0300

    BUG 006: Corrige menu da aplicação
    Descrição: Altera menu para que dê retorno visual de onde o usuário se encontra na aplicação

--- usuarios/views.py
+++ usuarios/views.py
@@ -88,7 +88,7 @@ def usuario_cadastro(request, template_name="usuario_cadastro.html"):
         messages.error(request, "Permissão Negada!")
         return redirect('/projeto_lista/')
 
-    return render(request, template_name, {})
+    return render(request, template_name, {"menu":"configuracoes"})
 
 @login_required
 def usuario_editar(request, pk, template_name='usuario_editar.html'):
@@ -123,7 +123,7 @@ def usuario_editar(request, pk, template_name='usuario_editar.html'):
         messages.error(request, "Permissão Negada!")
         return redirect('/projeto_lista/')
 
-    return render(request, template_name, {'usuario': usuario})
+    return render(request, template_name, {'usuario': usuario, "menu":"configuracoes"})
 
 @login_required
 def usuario_delete(request):
@@ -151,7 +151,7 @@ def usuario_configuracao(request, template_name='usuario_configuracao.html'):
         tema = request.POST['tema']
         usuario.profile.tema = tema
         usuario.save()
-    return render(request, template_name)
+    return render(request, template_name, {"menu":"configuracoes"})
 
 @login_required
 def redefinicao_senha_admin(request):
@@ -201,7 +201,7 @@ def redefinicao_senha(request):
 def usuario_lista(request, template_name="usuario_lista.html"):
     if request.user.is_staff:
         usuarios = User.objects.all()
-        usuario = {'lista': usuarios}
+        usuario = {'lista': usuarios, "menu":"configuracoes"}
         return render(request, template_name, usuario)
     else:
         messages.error(request, "Permissão Negada!")
@@ -231,7 +231,7 @@ def projeto_cadastro(request, template_name="projeto_cadastro.html"):
 
         # Redirecionamento
         return redirect('/projeto_lista/')
-    return render(request, template_name, {'membro_list':membro})
+    return render(request, template_name, {'membro_list':membro, "menu":"projetos"})
 
 @login_required
 def projeto_editar(request, pk, template_name="projeto_editar.html"):
@@ -251,7 +251,7 @@ def projeto_editar(request, pk, template_name="projeto_editar.html"):
     else:
         messages.error(request, "Permissão Negada!")
         return redirect('/projeto_lista/')
-    return render(request, template_name, {'projeto': form, 'membro_list':membro})
+    return render(request, template_name, {'projeto': form, 'membro_list':membro, "menu":"projetos"})
 
 @login_required
 def projeto_delete(request):
@@ -279,7 +279,7 @@ def projeto_lista(request, template_name="projeto_lista.html"):
         projetos = Projeto.objects.all()
     else:
         projetos = Projeto.objects.filter(membros__user_id=user.id)
-    context = {"context": projetos}
+    context = {"context": projetos, "menu":"projetos"}
     return render(request, template_name, context)
 
 
@@ -309,7 +309,7 @@ def tarefa_cadastro(request, pk, template_name="tarefa_cadastro.html"):
 
         # Redirecionamento
         return redirect('/visao_projeto/' + pk)
-    context = {'form': tarefa, 'membro_list':membro, 'categoria_list':categoria, 'projeto':projeto}
+    context = {'form': tarefa, 'membro_list':membro, 'categoria_list':categoria, 'projeto':projeto, "menu":"projetos"}
     return render(request, template_name, context)
 
 
@@ -337,7 +337,7 @@ def tarefa_editar(request, pk, template_name="tarefa_editar.html"):
 
             return redirect('/visao_projeto/' + projeto.id.__str__())
 
-    context = {'membro_list': membro, 'categoria_list': categoria, 'projeto': projeto, 'tarefa':tarefa, 'data_limite_tratada':data_limite_tratada}
+    context = {'membro_list': membro, 'categoria_list': categoria, 'projeto': projeto, 'tarefa':tarefa, 'data_limite_tratada':data_limite_tratada, "menu":"projetos"}
     return render(request, template_name, context)
 
 
@@ -435,20 +435,40 @@ def projeto_visao(request, pk, template_name="visao_projeto.html"):
         messages.error(request, "Projeto não encontrado!")
         return redirect('/projeto_lista/')
 
-    context = {'projeto': projeto, 'tarefas': tarefas, 'categorias': categoria, 'template': "visao_projeto"}
+    context = {'projeto': projeto, 'tarefas': tarefas, 'categorias': categoria, 'template': "visao_projeto",  "menu":"projetos" }
     return render(request, template_name, context)
 
 @login_required
 def tarefas_do_usuario_projeto(request, pk, template_name="visao_projeto.html"):
     tarefas = Tarefa.objects.filter(Q(resp=request.user.membro.id) & Q(projeto_id=pk))
     categoria = Categoria.objects.all()
+
+    for item in categoria:
+        count_total = 0
+        count_concluido = 0
+
+        for tarefa in tarefas:
+            if item.id == tarefa.categoria.id:
+                count_total += 1
+                if tarefa.concluido:
+                    count_concluido += 1
+
+        if count_total == 0:
+            item.porcentagem = -1
+        else:
+            if count_concluido == 0:
+                item.porcentagem = 0
+            else:
+                percent = 100 * (count_concluido/count_total)
+                item.porcentagem = percent
+
     try:
         projeto = Projeto.objects.get(pk=pk)
     except:
         messages.error(request, "Projeto não encontrado!")
         return redirect('/projeto_lista/')
 
-    context = {'projeto': projeto, 'tarefas': tarefas, 'categorias': categoria, 'template': "visao_projeto_tarefa"}
+    context = {'projeto': projeto, 'tarefas': tarefas, 'categorias': categoria, 'template': "visao_projeto_tarefa",  "menu":"projetos" }
     return render(request, template_name, context)
 
 @login_required
@@ -462,7 +482,7 @@ def tarefa_visao(request, template_name="visao_tarefas.html"):
     tarefas = Tarefa.objects.all()
     categorias = Categoria.objects.all()
 
-    context = {"projetos": projetos, "tarefas":tarefas, "categorias":categorias}
+    context = {"projetos": projetos, "tarefas":tarefas, "categorias":categorias, "menu":"tarefas"}
     return render(request, template_name, context)
 
 @login_required
@@ -475,5 +495,5 @@ def visao_requisitos(request, pk, template_name="visao_requisitos.html"):
 
     requisitos = Requisito.objects.filter(projeto_id=projeto.pk)
 
-    context = {"projeto": projeto, "requisitos":requisitos, "template":"visao_requisito" }
+    context = {"projeto": projeto, "requisitos":requisitos, "template":"visao_requisito", "menu":"projetos" }
     return render(request, template_name, context)
