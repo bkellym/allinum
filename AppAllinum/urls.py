a8dbb93cce20f4d80dec92e453d8c74af1e4da4c
bkellym <bkelly.sm@gmail.com>
2020-06-02 19:35:42 -0300

    Conclui as alterações do commit anterior.

--- AppAllinum/urls.py
+++ AppAllinum/urls.py
@@ -13,11 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
 from django.conf.urls import url
-from django.urls import path, include
+from django.urls import include
 
 urlpatterns = [
     #    path('admin/', admin.site.urls),
-    url(r'^', include('usuarios.urls'))
+    url(r'^', include('core.urls'))
 ]
